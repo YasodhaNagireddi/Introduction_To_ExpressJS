e80dd0d9bbb93b1b99df8fa9ff3c4c12b01e1da5
Yasodha21 <yasodhanagireddi21@gmail.com>
2022-11-02 15:43:19 +0000

    Introduction to ExpressJS

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express();
 
 app.get("/", (request, response) => {
   response.send("Hello World!");
+  console.log(request);
 });
 
 app.get("/date", (request, response) => {
   let date = new Date();
   response.send(`Today's date is ${date}`);
+  console.log(response);
 });
 
+app.get("/page", (request, response) => {
+  response.sendFile("/page.html", { root: __dirname });
+});
 app.listen(3000);
